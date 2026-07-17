0bce26b41477a9b631c22feabb0f6252f8ce2983
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-07-17 18:35:39 +0530

    Time: 2 ms (82.4%), Space: 13.8 MB (61.96%) - LeetHub - Time: O(n), Space: O(1)

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -3,12 +3,9 @@ public:
     long long countVowels(string word) {
         long long count = 0;
         long long n = word.size();
-        long long total_SS = (n%2==0) ? (n/2)*(n+1) : n*(n+1)/2;
         for(int i = 0; i < n ; ++i){
             if( word[i] == 'a'|| word[i]== 'e'|| word[i]== 'i'|| word[i]== 'o'|| word[i]== 'u'){
-                long long red_up = (n-i-1)*(n-i)/2; 
-                long long red_down = (static_cast<long long>(i) * (i + 1)) / 2;
-                count += (total_SS - red_up - red_down);
+                count += (i+1)*(n-i);
             }
         }
         return count;
