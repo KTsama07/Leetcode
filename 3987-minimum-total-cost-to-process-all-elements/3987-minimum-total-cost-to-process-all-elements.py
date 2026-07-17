73589258423f15fcda2b0adc7f1f3a158bd8ef50
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-07-17 12:28:10 +0530

    Delete 3987-minimum-total-cost-to-process-all-elements directory

--- 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def minimumCost(self, nums: list[int], k: int) -> int:
-        z = sum(nums)
-        n = z//k
-        if(z%k==0):
-            n = n-1
-        cost = int(n*(n+1)//2)
-        return cost%(10**9+7)
