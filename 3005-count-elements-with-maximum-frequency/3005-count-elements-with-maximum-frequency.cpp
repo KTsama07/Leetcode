06ce34a07c32c3684b4205a93c70b440e7cd4bee
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-09 20:38:02 +0530

    Time: 0 ms (100%), Space: 23.2 MB (68.21%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int hash[101] = {0};
+        for(int i=0; i<nums.size();i++){
+            hash[nums[i]] +=1;
+        }
+        sort(hash,hash+101);
+        int t = 0;
+        for(int i=0; i<101;i++){
+            if(hash[i]==hash[100]){
+                t = t + hash[i];
+            }
+        }
+        return t;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
