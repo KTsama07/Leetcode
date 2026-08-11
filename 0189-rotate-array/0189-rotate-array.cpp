05c57cb9b8b2cdf4670e0ba343a41e7663104c3a
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-12 05:10:39 +0530

    //optimal space wise

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,16 +4,9 @@ public:
         int n = nums.size();
         k = k % n;
         k = n-k;
-        vector<int> temp(k);
-        for(int i = 0 ; i < k; i++){
-            temp[i] = nums[i];
-        }
-        for(int i=k; i<n; i++){
-            nums[i-k]= nums[i];
-        }
-        for(int i = n-k; i < n; i++){
-            nums[i] = temp[i-(n-k)];
-        }
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+        reverse(nums.begin(), nums.end());
     }
 };
 
