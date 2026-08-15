8821e00ba5cfa84d739be4e337b6bd5a73d675ef
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-16 03:07:50 +0530

    Time: 0 ms (100%), Space: 12.4 MB (36.92%) - LeetHub - Time: O(m + n), Space: O(1)

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0 ;
-        int j = 0 ;
-        vector<int> temp;
-        while( i < m && j<n){
-            if(nums1[i]<nums2[j]){
-                temp.push_back(nums1[i]);
-                i++;
-            } else{
-                temp.push_back(nums2[j]);
-                j++;
+        int i = m-1 ;
+        int j = n-1 ;
+        int k = m+n-1;
+        while(j>=0){
+            if(i >=0 && nums1[i]>nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
             }
-        }
-        while(j<n){
-            temp.push_back(nums2[j]);
-            j++;
-        }
-        while(i<m){
-            temp.push_back(nums1[i]);
-            i++;
-        }
-        for(int k = 0 ; k < nums1.size();k++){
-            nums1[k] = temp[k];
+            k--;
         }
     }
 };
