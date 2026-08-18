1ce16ebe20811b1f70408b53a5c43a96c0b00c61
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-18 23:08:56 +0530

    Time: 0 ms (100%), Space: 20.6 MB (59.14%) - LeetHub - Time: O(n), Space: O(1)

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int xor1 = 0;
-        for(int i =0 ; i < nums.size() ; i++){
-            xor1 ^= nums[i];
+        for(int i : nums){
+            xor1 ^= i;
         }
         return xor1;
     }
