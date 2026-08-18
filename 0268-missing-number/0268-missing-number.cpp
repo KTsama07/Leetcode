a681be703a1c754133fadeb65a7ccc1171773a16
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-18 22:46:19 +0530

    Time: 0 ms (100%), Space: 21.8 MB (38.96%) - LeetHub - Time: O(n), Space: O(1)

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int xor1 = 0;
-        int xor2 = 0;
+        //int xor2 = 0;
         int n = nums.size();
         for(int i = 1 ; i <= n ; i++){
             xor1 ^= i;
         }
         for(int i = 0 ; i < n ; i++){
-            xor2 ^= nums[i];
+            xor1 ^= nums[i];
         }
-        int ans = xor1 ^ xor2;
-        return ans;
+        //int ans = xor1 ^ xor2;
+        return xor1;
     }
 };
 
