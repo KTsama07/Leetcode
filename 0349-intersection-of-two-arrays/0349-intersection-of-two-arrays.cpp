56aced40322d02bd58a3c78b46b63a199999ec24
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-18 22:27:31 +0530

    Time: 0 ms (100%), Space: 14.3 MB (45.8%) - LeetHub - Time: O((n + m) log n), Space: O(n)

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        set<int> st;
+        set<int> st(nums1.begin(), nums1.end());
         vector<int> result;
         int n = nums1.size();
         int m = nums2.size();
-        for(int i = 0; i<n ;i++){
-            st.insert(nums1[i]);
-        }
+        // for(int i = 0; i<n ;i++){
+        //     st.insert(nums1[i]);
+        // }
         for(int j = 0; j<m ; j++){
                 if(st.count(nums2[j])){
                     result.push_back(nums2[j]);
