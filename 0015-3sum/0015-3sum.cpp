e4d5c27b2cdded740ca5bc593b92bf8d0f58b50f
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-22 20:35:22 +0530

    Time: 59 ms (19.33%), Space: 30.9 MB (23.44%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+          vector<vector<int>> ans;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+                int k = i+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == 0){
+                        vector<int> temp = {nums[i], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    }
+                    else if( sum > 0) l--;
+                    else k++;
+                }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
