33424b6b583d96e589cc9d098750c10a22cb32c4
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-24 01:53:29 +0530

    Time: 53 ms (94.41%), Space: 243 MB (49.47%) - LeetHub

--- 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/4031-find-all-numbers-disappeared-in-an-array-ii.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
-     set<int> st(nums.begin(), nums.end());
+     vector<bool> present(upper+1, false);
+     for(auto x: nums) if(x>=lower && x<= upper) present[x] = true;
      vector<vector<int>> ans;
-     int start = -1;
-     for(int i=lower; i <= upper; ++i){
-        if(st.find(i) == st.end()) {
-            //strart of missing range
-            if(start == -1){
-                start = i;
-            }
-        } else {
-            if( start != -1){
-                ans.push_back({start , i-1});
-                start = -1;
-            }
-
-        }
-        
-     }
-     if(start != -1){
-        ans.push_back({start,upper});
-     }   
+     for(int i=lower; i <= upper;){
+        if(present[i]) {
+            i++;
+            continue;
+        } 
+        int start = i;
+        while(i<= upper && !present[i]) ++i;
+        ans.push_back({start, i-1});
+     }  
      return ans;
     }
 };
