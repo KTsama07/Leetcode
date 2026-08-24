618fae74d1884b23ea6f0ee6be5ffea7dd67347c
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-24 17:26:39 +0530

    Time: 0 ms (100%), Space: 20.6 MB (36.65%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        low = 0
+        high = n-1
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid]> target:
+                high = mid-1
+            else : low = mid+1
+        return -1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
