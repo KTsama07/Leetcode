09c19af798d93b6d48a3495f2d8055e7031ae94b
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-24 21:50:14 +0530

    Time: 0 ms (100%), Space: 19.6 MB (31.72%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        low =0; high = n-1
+        while low<=high :
+            mid = (low+high)//2
+            if nums[mid] == target : return True
+            '''if boundaries are equal we can't determine which half is sorted'''
+            if nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+            elif nums[mid] >= nums[low]:
+                if nums[low]<= target <= nums[mid]:
+                    high = mid-1
+                else: low = mid+1
+            else :
+                if nums[mid]< target <= nums[high]:
+                    low = mid+1
+                else : high = mid - 1
+        return False
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
