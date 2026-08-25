cfcfd3d6b8baf7f6c50f304d0e73bd9089046084
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-25 15:48:40 +0530

    Time: 54 ms (55.59%), Space: 23.1 MB (7.01%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        ptset = set()
+        temp = head 
+        while temp:
+            if temp in ptset:
+                return True
+            ptset.add(temp)
+            temp = temp.next
+        return False
+        
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
