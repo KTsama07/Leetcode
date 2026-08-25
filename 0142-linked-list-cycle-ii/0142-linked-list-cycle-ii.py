5ed1b4f3a58448ddb6f276d080dbcbd9ed04c8d4
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-25 16:18:43 +0530

    Time: 50 ms (73.14%), Space: 22.8 MB (5.57%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        ptr_dict = {}
+        temp = head
+        index = 0
+        while temp:
+            if temp in ptr_dict:
+                return temp
+            ptr_dict[temp] = index
+            temp = temp.next
+            index = +1
+        return None
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
