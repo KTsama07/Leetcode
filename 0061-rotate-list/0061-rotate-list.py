5b171bdaee70609960c0da4658be35fc53b04346
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-28 01:52:47 +0530

    Time: 3 ms (14.83%), Space: 19.3 MB (77.01%) - LeetHub - Time: O(N^2), Space: O(1)

---
+++ 0061-rotate-list/0061-rotate-list.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return 
+        curr = head
+        prev = None
+        cnt = 1
+        while curr.next:
+            prev = curr
+            curr = curr.next
+            cnt += 1
+        k = k%cnt
+        while k != 0:
+            curr.next = head
+            head = curr
+            prev.next = None
+            prev_new = head
+            while prev_new.next.next:
+                prev_new = prev_new.next
+            curr = prev
+            prev = prev_new
+            k -= 1
+        return head
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
