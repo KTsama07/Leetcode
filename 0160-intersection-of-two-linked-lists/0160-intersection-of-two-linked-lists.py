c2247ee08dee5f61e2a32e03853c45c9d6d7290b
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-08-28 18:54:52 +0530

    Time: 114 ms (50.15%), Space: 39 MB (8.01%) - LeetHub - Time: O(N + M), Space: O(N + M)

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        x = headA
+        y = headB
+        l1 = []
+        l2 = []
+        while x or y:
+            if x : 
+                l1.append(x)
+                x = x.next
+            if y : 
+                l2.append(y)
+                y = y.next
+        s2 = set(l2)
+        result = [item for item in l1 if item in s2]
+        if result:
+            return result[0]
+        else:
+            return None
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
