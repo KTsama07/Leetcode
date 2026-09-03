10fff35122eeb108b9e97c91b0818f0ad4c84462
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-03 19:42:46 +0530

    Time: 0 ms (100%), Space: 20.6 MB (5.48%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(head == nullptr){
+            return nullptr;
+        }
+        while (head != nullptr && head->val == val){
+            head = head->next;
+        }
+        if(head != nullptr){
+            head->next = removeElements(head->next, val);
+        }
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
