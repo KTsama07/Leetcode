d8834ecca3ceb1628303affdfea763c6dedb62aa
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-04 09:45:52 +0530

    Time: 0 ms (100%), Space: 11.2 MB (20.2%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr){
+            return head;
+        }
+        ListNode* front = head->next->next;
+        ListNode* curr = head->next;
+        curr->next = head;
+        head->next = swapPairs(front);
+        head = curr;
+        return head;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
