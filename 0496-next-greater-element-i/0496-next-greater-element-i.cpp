3919663ffe1093b9caa0745d7bdf4b477574eb6c
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-11 17:20:09 +0530

    Time: 1 ms (54.04%), Space: 12.9 MB (38.68%) - LeetHub - Time: O(n + m), Space: O(n)

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        int n = nums2.size();
+        unordered_map<int,int> nge;
+        for(int i = n-1; i >= 0 ; i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+            nge[nums2[i]] = st.empty() ? -1 : st.top();
+            st.push(nums2[i]);
+        }
+        vector<int> ans;
+        for(auto num : nums1){
+            ans.push_back(nge[num]);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
