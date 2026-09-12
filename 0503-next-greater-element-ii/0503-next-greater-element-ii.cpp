65a784b8251f9a6207a2521ca7ade273d83ac735
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-12 19:35:15 +0530

    Time: 8 ms (25.53%), Space: 28.6 MB (21.88%) - LeetHub - Time: O(n), Space: O(n)

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
         int n = nums.size();
         vector<int> nge(n,-1);
-        for(int i = 0; i < n ;i++){
-            for(int j=i+1 ; j < i+n ; j++){
-                int ind = j % n ;
-                if(nums[ind] > nums[i]){
-                    nge[i] = nums[ind];
-                    break;
+        for(int i = 2*n-1; i>=0; i--){
+                while(!st.empty() && nums[i%n]>=st.top()){
+                    st.pop();
                 }
-            }
+                if(i<n) nge[i] = !st.empty() ? st.top() : -1;
+                st.push(nums[i%n]);
         }
-        // for(int i = n-1; i>=0; i--){
-        //         while(!st.empty() && nums[i]>=st.top()){
-        //             st.pop();
-        //         }
-        //         if(!st.empty()){
-        //             nge[i] = st.top();
-        //         }
-        //         st.push(nums[i]);
-        // }
-        // while(!st.empty()&&nums[n-1] >= st.top()){
-        //     st.pop();
-        // }
-        // nge[n-1] = !st.empty() ? st.top() : -1 ;
         return nge;
     }
 };
