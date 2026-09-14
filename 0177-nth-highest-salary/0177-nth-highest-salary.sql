be646111aff9070152800c9857f4b0dc87c993b2
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-14 23:40:22 +0530

    Time: 267 ms (19.04%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+    SELECT DISTINCT e.salary FROM Employee AS e
+    WHERE N > 0
+    ORDER BY salary DESC 
+    LIMIT 1 OFFSET GREATEST(N-1,0)
+      
+  );
+END;
+$$ LANGUAGE plpgsql;
+
+/*
+Synced seamlessly with LeetHub Pro
+Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+*/
