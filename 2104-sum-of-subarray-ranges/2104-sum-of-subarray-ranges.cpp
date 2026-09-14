277a85f7fb7ede12000e9fc79a2a8e5464c60be0
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-14 21:08:12 +0530

    Time: 4 ms (59.65%), Space: 14.2 MB (71.81%) - LeetHub - Time: O(n), Space: O(n)

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,17 +2,34 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long ans =0 ;
-        for(int i = 0 ; i < n ; i++){
-            int max_t = nums[i];
-            int min_t = nums[i];
-            for(int j = i+1; j<n ; j++){
-                max_t = max(max_t,nums[j]);
-                min_t = min(min_t,nums[j]);
-                ans += max_t-min_t;
+        long long min_sum = 0;
+        stack<int> st;
+        //find sum of subarray minimums
+        for(int i = 0 ; i <=n ; i++){
+            while(!st.empty() && (i==n || nums[st.top()] > nums[i])){
+                int mid = st.top();
+                st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int left_count = mid - left;
+                int right_count = i - mid;
+                min_sum += 1LL *  left_count * right_count * nums[mid];
             }
+            st.push(i);
         }
-        return ans;
+        while(!st.empty()) st.pop();
+        long long max_sum = 0;
+        for(int i = 0 ; i <= n ; i++){
+            while(!st.empty() && (i==n || nums[st.top()] < nums[i])){
+                int mid = st.top();
+                st.pop();
+                int left = st.empty() ? -1 : st.top();
+                int left_count = mid-left;
+                int right_count = i - mid;
+                max_sum += 1LL * left_count * right_count * nums[mid];
+            }
+            st.push(i);
+        }
+        return max_sum - min_sum;
     }
 };
 
