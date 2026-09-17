8bac0891296e53251429ac93302305ec0a43551b
Goku ka bhai <146992691+KTsama07@users.noreply.github.com>
2026-09-17 20:06:35 +0530

    Time: 53 ms (19.73%), Space: 84.8 MB (83.41%) - LeetHub - Time: O(n), Space: O(1)

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -7,12 +7,12 @@ public:
         unordered_map<int, int> mpp;
         while(r < fruits.size()){
             mpp[fruits[r]]++;
-            while(mpp.size() > 2){
+            if(mpp.size() > 2){
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
                 l++;
             }
-            max_len = max(max_len , r-l+1);
+            else max_len = max(max_len , r-l+1);
             r++;
         }
         return max_len;
